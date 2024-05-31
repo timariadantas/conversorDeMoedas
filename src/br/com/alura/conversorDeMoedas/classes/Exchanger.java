76889204379b50fb5bfffbb21de0b7782c6a148d
Maria Dantas <123247906+timariadantas@users.noreply.github.com>
2024-05-31 09:01:08 -0300

    Initial commit

---
+++ src/br/com/alura/conversorDeMoedas/classes/Exchanger.java
@@ -0,0 +1,2 @@
+package br.com.alura.conversorDeMoedas.classes;public class Exchanger {
+}
---
+++ src/br/com/alura/conversorDeMoedas/exceptions/InvalidCurrencyException.java
@@ -0,0 +1,2 @@
+package br.com.alura.conversorDeMoedas.exceptions;public class InvalidCurrencyException {
+}
